5709c8bbabb5e6288b3026a36fd0be2fbc530555
Vlad <nahabavladislav@gmail.com>
2020-02-20 15:23:05 +0200

    Bug fixed with init widgets

--- js/popular-widget-script.js
+++ js/popular-widget-script.js
@@ -1,21 +1,17 @@
 jQuery(function( $ ) {
-  $(document).ready(
-    $(document).on('ready widget-updated widget-added', function (event, widget) {
-      handle_widget_updated('.widget-popular-posts');
-    })
-  );
-  function handle_widget_updated(selector) {
-    $(selector + ' .popular-posts-select').select2();
-
-    $(selector + ' .popular-posts-category').select2();
-
-    $(selector + ' .popular-posts-tag').select2();
-
-    $(selector + ' .popular-posts-author').select2();
-
-    $(selector + ' .popular-posts-template').select2();
-
-    $(selector + ' .popular-posts-post').select2({
+  $(document).on('widget-updated widget-added', function(event, widget) {
+    handle_widget_updated($(widget));
+  });
+  $(document).on('ready', function (event, widget) {
+    handle_widget_updated($('#widgets-right'));
+  });
+  function handle_widget_updated($selector) {
+    $selector.find('.popular-posts-select').select2();
+    $selector.find('.popular-posts-category').select2();
+    $selector.find('.popular-posts-tag').select2();
+    $selector.find('.popular-posts-author').select2();
+    $selector.find('.popular-posts-template').select2();
+    $selector.find('.popular-posts-post').select2({
       ajax: {
         url: ajaxurl + '?action=popular_posts_widget',
         dataType: 'json',
@@ -23,6 +19,5 @@ jQuery(function( $ ) {
       },
       minimumInputLength: 3
     });
-
   }
 });
--- lib/dropdown-categories-multiple.php
+++ lib/dropdown-categories-multiple.php
@@ -1,11 +1,14 @@
 <?php
-add_filter( 'wp_dropdown_cats', 'wp_dropdown_cats_multiple', 10, 2 );
-function wp_dropdown_cats_multiple( $output, $r ) {
-	if( isset( $r['multiple'] ) && $r['multiple'] ) {
-		$output = preg_replace( '/^<select/i', '<select multiple', $output );
-		foreach ( array_map( 'trim', explode( ",", $r['selected'] ) ) as $value ) {
-			$output = str_replace( "value=\"{$value}\"", "value=\"{$value}\" selected", $output );
+if( is_admin() ) {
+	add_filter( 'wp_dropdown_cats', 'wp_dropdown_cats_multiple', 10, 2 );
+	function wp_dropdown_cats_multiple( $output, $r ) {
+		if ( isset( $r['multiple'] ) && $r['multiple'] ) {
+			$output = preg_replace( '/^<select/i', '<select multiple', $output );
+			foreach ( array_map( 'trim', explode( ",", $r['selected'] ) ) as $value ) {
+				$output = str_replace( "value=\"{$value}\"", "value=\"{$value}\" selected", $output );
+			}
 		}
+
+		return $output;
 	}
-	return $output;
 }
