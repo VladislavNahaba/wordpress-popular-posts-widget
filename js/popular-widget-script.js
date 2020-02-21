f4d04fcfb5d7661fd22b8f63f43740830e4f7997
Vlad <nahabavladislav@gmail.com>
2020-02-21 14:45:24 +0200

    bug fixes, bad init select2

--- js/popular-widget-script.js
+++ js/popular-widget-script.js
@@ -6,18 +6,31 @@ jQuery(function( $ ) {
     handle_widget_updated($('#widgets-right'));
   });
   function handle_widget_updated($selector) {
-    $selector.find('.popular-posts-select').select2();
-    $selector.find('.popular-posts-category').select2();
-    $selector.find('.popular-posts-tag').select2();
-    $selector.find('.popular-posts-author').select2();
-    $selector.find('.popular-posts-template').select2();
-    $selector.find('.popular-posts-thumbnail').select2();
+    $selector.find('.popular-posts-select').select2({
+      width: '100%'
+    });
+    $selector.find('.popular-posts-category').select2({
+      width: '100%'
+    });
+    $selector.find('.popular-posts-tag').select2({
+      width: '100%'
+    });
+    $selector.find('.popular-posts-author').select2({
+      width: '100%'
+    });
+    $selector.find('.popular-posts-template').select2({
+      width: '100%'
+    });
+    $selector.find('.popular-posts-thumbnail').select2({
+      width: '100%'
+    });
     $selector.find('.popular-posts-post').select2({
       ajax: {
         url: ajaxurl + '?action=popular_posts_widget',
         dataType: 'json',
         delay: 250
       },
+      width: '100%',
       minimumInputLength: 3
     });
   }
--- lib/dropdown-categories-multiple.php
+++ lib/dropdown-categories-multiple.php
@@ -4,6 +4,7 @@ if( is_admin() ) {
 	function wp_dropdown_cats_multiple( $output, $r ) {
 		if ( isset( $r['multiple'] ) && $r['multiple'] ) {
 			$output = preg_replace( '/^<select/i', '<select multiple', $output );
+			$output = preg_replace( '/name=([\'"]{1})(.*?)\1/i', 'name=$2[]', $output );
 			foreach ( array_map( 'trim', explode( ",", $r['selected'] ) ) as $value ) {
 				$output = str_replace( "value=\"{$value}\"", "value=\"{$value}\" selected", $output );
 			}
